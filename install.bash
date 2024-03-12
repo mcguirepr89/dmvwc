c5715c99c004e7bf766a482d30f831cc3538e3e9
mcguirepr89 <mcguirepr89@gmail.com>
2024-03-12 18:08:33 -0400

    testing out a small developer's installation script

---
+++ install.bash
@@ -0,0 +1,37 @@
+mkdir ~/WatchCollectionProject && git clone https://github.com/mcguirepr89/dmvwc.git ~/WatchCollectionProject/dmvwc
+
+cd ~/WatchCollectionProject && python3 -m venv venv
+
+cd ~/WatchCollectionProject/dmvwc && source ../venv/bin/activate
+
+pip install -U pip
+
+pip install -r requirements.txt
+
+wget https://github.com/tailwindlabs/tailwindcss/releases/download/v3.0.23/tailwindcss-linux-x64 && mv tailwindcss-linux-x64 ../venv/bin/tailwindcss
+
+chmod +x ~/WatchCollectionProject/venv/bin/tailwindcss
+
+cat > ~/WatchCollectionProject/dmvwc/.env <<EOF
+# .env
+SECRET_KEY=django-insecure-mySuperLongSecretKeyThatNeedsToBeChangedForProduction
+DEBUG=True
+ALLOWED_HOSTS=*
+EOF
+
+
+mkdir -p ~/WatchCollectionProject/dmvwc/{static/css,media}
+
+cat > ~/WatchCollectionProject/dmvwc/static/css/input.css <<EOF
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+EOF
+
+
+tailwindcss -i ~/WatchCollectionProject/dmvwc/static/css/input.css -o ~/WatchCollectionProject/dmvwc/static/css/output.css --watch
+
+python manage.py migrate --fake-initial
+python manage.py migrate
+
+python manage.py runserver
